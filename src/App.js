5eb8e362a36a887c551232995968da261d7d4c15
LivingstonYui <innovation_yui@163.com>
2017-12-17 21:14:50 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="stage">
+        <section className="img-sec">
+          1111
+        </section>
+        <nav className="controller-nav"></nav>
+      </div>
+    );
+  }
+}
+
+export default App;
